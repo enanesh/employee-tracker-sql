c24877e3d26bdc4b73d6e54a1f5af8430553d89d
enanesh <edna.ganesha.gr@gmail.com>
2022-12-16 02:09:23 -0800

    Changes to schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -3,43 +3,34 @@ CREATE DATABASE employeeTracker_db;
 
 USE employeeTracker_db;
 
-
---CREATES DEPARTMENT TABLE
 CREATE TABLE department(
-    id INT NOT NULL ,
+    id INT NOT NULL,
     department_name VARCHAR(30) NOT NULL,
-    --KEYS
-    PRIMARY KEY (id)
-);
 
-
---CREATES R0LE TABLE
-
-CREATE TABLE role(
-    id INT NOT NULL AUTO_INCREMENT ,
-    tittle VARCHAR(30),
-    salary DECIMAL,
-    department_id INT NOT NULL,
-    --KEYS
-    PRIMARY KEY (id)
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
-    ON DELETE SET NULL
-);
-
-
---CREATES EMPLOYEE TABLE 
-
-CREATE TABLE employee(
-    id INT NOT NULL AUTO_INCREMENT ,
-    first_name VARCHAR(30), 
-    last_name VARCHAR(30),
-    role_id INT NOT NULL,
-    manager_id INT REFERENCES employee(id),
-    --KEYS
     PRIMARY KEY (id),
-    FOREIGN KEY (role_id),
-    REFERENCES role(id)
-
+    UNIQUE KEY (department_name)
 );
 
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) 
+  REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT REFERENCES employee(id),
+
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) 
+  REFERENCES role(id)
+
+);   
--- db/seeds.sql
+++ db/seeds.sql
@@ -6,7 +6,7 @@ VALUES (1, 'Sales'),
 
 
 
-INSERT INTO role (tittle,salary,department_id)
+INSERT INTO role (title,salary,department_id)
 VALUES ('Sales Lead',100000,1),
        ('Sales Salesperson',80000,1),
        ('Lead Engineer',150000,2),
