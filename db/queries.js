e76cd355aee64b421ebea0574bde16b387367a61
enanesh <edna.ganesha.gr@gmail.com>
2022-12-18 01:38:05 -0800

    Changes to queries and AddRole prompt

--- db/queries.js
+++ db/queries.js
@@ -1,46 +1,41 @@
 const dbConnection = require('../lib/connection');
 const cTable = require('console.table');
 
-class dbQueries {
-    constructor(dbConnection) {
-        this.dbConnection = dbConnection;
-    }
 
-
-
-
-// dbConnection.query("SELECT * FROM students", function (err, results) {
-//     console.log(results);
-
-
-// });
-
-
-
-    // getDepts() {
-    //     return this.db
-    //         .promise()
-    //         .query(
-    //             `SELECT * 
-    // FROM department`
-    //         );
-    // }
- 
-    
-
+//ALL DEPARTMENTS TABLE
+function getDepartment() {
+    return dbConnection.promise().query(
+        'SELECT * FROM department ORDER BY id ASC');
 };
 
-function getDepartment() {
+// getDepartment();
+
+
+//ALL ROLES TABLE
+function getRoles() {
+    return dbConnection.promise().query(
+        'SELECT role.id,role.title,department.department_name AS Department,CONCAT("$",FORMAT(role.salary,"C0") ) AS Salary  FROM role JOIN department ON role.department_id = department.id  ORDER BY id ASC  ');
+};
+
+
+//ALL EMPLOYEES TABLE
+
+
+function getEmployees() {
     return dbConnection.query(
-        'SELECT * FROM department ORDER BY id ASC',
-        function (err, results, fields) {
+        'SELECT employee.id,employee.first_name,employee.last_name,department.department_name AS Deparment,CONCAT("$",FORMAT(role.salary,"C0")) AS salary,role.title,CONCAT(mgt.first_name," ", mgt.last_name) AS Manager FROM employee LEFT JOIN employee mgt ON employee.manager_id = mgt.id INNER JOIN role ON employee.role_id = role.id  LEFT JOIN  department ON role.department_id = department.id ORDER BY employee.id ASC ', function (err, results, fields) {
             console.log(cTable.getTable(results)); // results contains rows returned by server
 
         }
     )
 };
 
-getDepartment();
+// getEmployees()
 
 
-module.exports = {dbQueries};
+function addRole(roleName,roleSalary,departmentId) {
+    dbConnection.query(`INSERT INTO role(title,salary,department_id) VALUES ("${roleName}",${roleSalary},${departmentId})`);
+}
+
+
+module.exports = {  getRoles, getDepartment,addRole };
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const inquirer = require("inquirer");
 const cTable = require('console.table');
 const sql = require('./db/queries');
+const ascii_text_generator = require('ascii-text-generator');
+
+console.log(ascii_text_generator("te amo", "2"))
 
 
-
-
-const promtQuestions = () => {
+const promptActions = () => {
     return inquirer.prompt([
         {
             type: 'list',
@@ -18,29 +19,76 @@ const promtQuestions = () => {
                 'Add Role',
                 'View All Departments',
                 'Add department']
-                  
-                
+
+
+        }]).then(answer => {
+
+            switch (answer.request) {
+                case "View All Roles":
+                    sql.getRoles().then(([results]) => {
+                        console.log('\n');
+                        console.log(cTable.getTable(results));
+                    });
+                    break;
+                case "Add Role":
+                    sql.getDepartment().then(([results]) => {
+                        addRole(results);
+                    });
+                    break;
+
+            }
+
+
+
+        })
+}
+
+
+const addDepartment = () => {
+    return inquirer.prompt([{
+        type: "input",
+
+    }])
+}
+
+
+
+
+/
+const addRole = (departments) => {
+    const departmentName = [];
+    // Get an array with only department names
+    for (let i = 0; i < departments.length; i++) {
+        let value = { "name": departments[i].department_name, "value": departments[i].id }
+        departmentName.push(value);
+    }
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the name of the role? ",
+            name: "roleName",
+
+        },
+        {
+            type: "input",
+            message: "What is the salary of the role? ",
+            name: "roleSalary",
+
+        },
+        {
+            type: 'list',
+            name: 'department',
+            message: 'What would you like to do?',
+            choices: departmentName
+
+
         }
 
     ]).then(answer => {
-        viewDepts();
-        
-
+        console.log(answer);
+        sql.addRole(answer.roleName,answer.roleSalary,answer.department);
     })
-}
 
+};
 
-const viewDepts = () => {
-    sql.getDepartment();
-    .then(([rows]) => {
-            console.log('\n');
-            console.log(cTable.getTable(rows));
-    });
-
-        
-}
-
-
-promtQuestions();
-
-console.log(sql);
+promptActions();
